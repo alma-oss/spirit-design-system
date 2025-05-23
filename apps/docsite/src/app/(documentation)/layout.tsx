35dca9076f399edb19abc70b2a4d15fa357ef04f
literat <tomas@litera.me>
2025-05-23 15:48:50 +0200

    feat(docsite): add site Header
    refs #DS-2446

--- apps/docsite/src/app/(documentation)/layout.tsx
+++ apps/docsite/src/app/(documentation)/layout.tsx
@@ -1,7 +1,31 @@
 'use client';
 
+import { Container, Footer } from '@alma-oss/spirit-web-react';
+import useIsPage from '@local/hooks/useIsPage';
+// import { Cover } from '@local/ui/Cover';
+import { Header } from '@local/ui/Header';
 import React, { ReactNode } from 'react';
 
-const DocumentationLayout = ({ children }: { children: ReactNode }) => <main>{children}</main>;
+const DocumentationLayout = ({ children }: { children: ReactNode }) => {
+  const isComponentsPage = useIsPage('components');
+
+  return (
+    <>
+      <Header />
+      <main>
+        {/* <Cover /> */}
+        {children}
+      </main>
+      <Footer
+        backgroundColor="secondary"
+        marginTop={{ mobile: 'space-1200', tablet: 'space-1200' }}
+        textAlignment="center"
+        {...(isComponentsPage && { UNSAFE_className: 'hide-from-visual-tests' })}
+      >
+        <Container>© Alma Career Oy and its subsidiaries</Container>
+      </Footer>
+    </>
+  );
+};
 
 export default DocumentationLayout;
