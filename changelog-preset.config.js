0e810024f0db973c64256b379f37dfaa95e3fc85
literat <tomas@litera.me>
2026-02-17 19:25:41 +0100

    chore(repo): introduce CJS changelog preset due to the compatibility with lerna
      * @see: https://github.com/lerna/lerna/issues/4021
      * lerna does not work with the versions of conventional-changelog-conventionalcommits
        form version 8 up due to the switch from CJS to ESM

---
+++ changelog-preset.config.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * Changelog preset configuration for Lerna.
+ *
+ * This file uses conventional-changelog-conventionalcommits@7.x which is
+ * CommonJS-compatible. Version 8.x+ is ESM-only and cannot be loaded by
+ * Lerna 9.x which uses require().
+ *
+ * @see https://github.com/lerna/lerna/issues/4021
+ */
+const config = require('conventional-changelog-conventionalcommits');
+
+module.exports = config({
+  types: [
+    { type: 'feat', section: 'Features', hidden: false },
+    { type: 'fix', section: 'Bug Fixes', hidden: false },
+    { type: 'revert', section: 'Reverts', hidden: false },
+    { type: 'docs', section: 'Documentation', hidden: false },
+    { type: 'deps', section: 'Dependencies', hidden: false },
+    { type: 'chore', section: 'Chores', hidden: false },
+    { type: 'perf', section: 'Performance Improvements', hidden: false },
+    { type: 'style', section: 'Styles', hidden: false },
+    { type: 'refactor', section: 'Code Refactoring', hidden: false },
+    { type: 'test', section: 'Tests', hidden: false },
+    { type: 'build', section: 'Build System', hidden: true },
+    { type: 'ci', section: 'Continuous Integration', hidden: true },
+  ],
+});
