a57301de5dd42b8f2cfa346e845eaf54bede9de8
Kateřina Řehořková <149075264+katerinarehorkova@users.noreply.github.com>
2026-02-20 10:47:05 +0100

    test(ci): test chromatic run - delete when done

--- packages/web-react/src/components/UNSTABLE_Header/stories/UNSTABLE_Header.stories.tsx
+++ packages/web-react/src/components/UNSTABLE_Header/stories/UNSTABLE_Header.stories.tsx
@@ -7,6 +7,8 @@ import ReadMe from '../README.md';
 import UNSTABLE_Header from '../UNSTABLE_Header';
 import UNSTABLE_HeaderLogo from '../UNSTABLE_HeaderLogo';
 
+// @TODO: test run chromatic - delete when done
+
 const meta: Meta<typeof UNSTABLE_Header> = {
   title: 'Experimental/UNSTABLE_Header',
   component: UNSTABLE_Header,
