972a68d4a8801338d381416fd47d47f95ffa9e94
Kateřina Dlouhá <katerina.dlouha@almamedia.com>
2026-02-27 12:25:55 +0100

    style(examples): remove unexpected block statement surrounding arrow body

--- examples/next-with-app-router/src/app/layout.tsx
+++ examples/next-with-app-router/src/app/layout.tsx
@@ -1,12 +1,10 @@
 import { ReactNode } from 'react';
 import './globals.scss';
 
-const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
-};
+const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => (
+  <html lang="en">
+    <body>{children}</body>
+  </html>
+);
 
 export default RootLayout;
