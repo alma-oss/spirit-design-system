496f6325d895aabbca27dd3a601e157c449f674f
Kateřina Řehořková <149075264+katerinarehorkova@users.noreply.github.com>
2026-04-01 12:15:20 +0200

    feat(web-react): align DropdownPopover props with standard div attributes #DS-2359

--- packages/web-react/src/components/Dropdown/__tests__/DropdownPopover.test.tsx
+++ packages/web-react/src/components/Dropdown/__tests__/DropdownPopover.test.tsx
@@ -27,4 +27,18 @@ describe('DropdownPopover', () => {
 
     expect(popover).toHaveTextContent('Popover');
   });
+
+  it('should not set role when role prop is omitted', () => {
+    const dom = render(<DropdownPopover>Popover</DropdownPopover>);
+    const popover = dom.container.querySelector('.DropdownPopover') as HTMLElement;
+
+    expect(popover).not.toHaveAttribute('role');
+  });
+
+  it('should set role when role prop is provided', () => {
+    const dom = render(<DropdownPopover role="dialog">Popover</DropdownPopover>);
+    const popover = dom.container.querySelector('.DropdownPopover') as HTMLElement;
+
+    expect(popover).toHaveAttribute('role', 'dialog');
+  });
 });
