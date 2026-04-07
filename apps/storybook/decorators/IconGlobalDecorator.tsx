1a3d0634af725cfd5a928bb066e009be0e58f738
literat <tomas@litera.me>
2026-04-07 16:33:58 +0200

    style(repo): remove ts ignore comments from Storybook's icons decorator

--- apps/storybook/decorators/IconGlobalDecorator.tsx
+++ apps/storybook/decorators/IconGlobalDecorator.tsx
@@ -1,10 +1,5 @@
-// Because there is no `dist` directory during the CI run
-/* eslint-disable import/no-extraneous-dependencies, import/extensions, import/no-unresolved */
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore: No declaration file
 import type { Decorator } from '@storybook/react';
 import { IconsProvider } from '@alma-oss/spirit-web-react';
-// @ts-ignore: No declaration file
 import icons from '@alma-oss/spirit-icons/icons';
 
 // Add debug logging to help identify deployment issues
