f595100e19a3c5381c047e7d57d860a49804dd51
Kateřina Klouček Dlouhá <katerina.kloucek.dlouha@almamedia.com>
2026-04-16 19:16:07 +0200

    fix(web-react): fix invalid Figma Code Connect for various components

--- packages/web-react/src/components/EmptyState/figma/EmptyState.figma.tsx
+++ packages/web-react/src/components/EmptyState/figma/EmptyState.figma.tsx
@@ -11,209 +11,55 @@ import EmptyStateSection from '../EmptyStateSection';
 const EMPTY_STATE_NODE_URL = '<FIGMA_FILE_ID>?node-id=19110%3A1243';
 
 figma.connect(EmptyState, EMPTY_STATE_NODE_URL, {
-  props: {},
-  variant: {
-    'Show Buttons': false,
-    'Show Illustration': false,
-    'Show Link': false,
-  },
-  example: (props) => (
-    <EmptyState {...props}>
-      <EmptyStateSection>
-        <Heading elementType="h2">Headline</Heading>
+  props: {
+    artwork: figma.boolean('Show Artwork', {
+      true: <EmptyStateSection>Replace with your own illustration content</EmptyStateSection>,
+      false: undefined,
+    }),
+    buttons: figma.boolean('Show Buttons', {
+      true: (
+        <EmptyStateSection>
+          <ActionGroup alignmentX={{ mobile: 'stretch', tablet: 'center' }}>
+            <ButtonLink href="#">Button</ButtonLink>
+            <ButtonLink href="#" color="secondary">
+              Button
+            </ButtonLink>
+          </ActionGroup>
+        </EmptyStateSection>
+      ),
+      false: undefined,
+    }),
+    headline: figma.boolean('Headline', {
+      true: <Heading elementType="h2">Headline</Heading>,
+      false: undefined,
+    }),
+    link: figma.boolean('Show Link', {
+      true: (
+        <EmptyStateSection>
+          <Link href="#link">Link to something</Link>
+        </EmptyStateSection>
+      ),
+      false: undefined,
+    }),
+    paragraph: figma.boolean('Paragraph text', {
+      true: (
         <Text>
           In publishing and graphic design, lorem ipsum is common placeholder text used to demonstrate the graphic
           elements
         </Text>
-      </EmptyStateSection>
-    </EmptyState>
-  ),
-});
-
-figma.connect(EmptyState, EMPTY_STATE_NODE_URL, {
-  props: {},
-  variant: {
-    'Show Buttons': true,
-    'Show Illustration': false,
-    'Show Link': false,
+      ),
+      false: undefined,
+    }),
   },
-  example: (props) => (
+  example: ({ artwork, buttons, headline, link, paragraph, ...props }) => (
     <EmptyState {...props}>
+      {artwork}
       <EmptyStateSection>
-        <Heading elementType="h2">Headline</Heading>
-        <Text>
-          In publishing and graphic design, lorem ipsum is common placeholder text used to demonstrate the graphic
-          elements
-        </Text>
+        {headline}
+        {paragraph}
       </EmptyStateSection>
-      <EmptyStateSection>
-        <ActionGroup alignmentX={{ mobile: 'stretch', tablet: 'center' }}>
-          <ButtonLink href="#">Button</ButtonLink>
-          <ButtonLink href="#" color="secondary">
-            Button
-          </ButtonLink>
-        </ActionGroup>
-      </EmptyStateSection>
-    </EmptyState>
-  ),
-});
-
-figma.connect(EmptyState, EMPTY_STATE_NODE_URL, {
-  props: {},
-  variant: {
-    'Show Buttons': false,
-    'Show Illustration': true,
-    'Show Link': false,
-  },
-  example: (props) => (
-    <EmptyState {...props}>
-      <EmptyStateSection>Replace with your own illustration content</EmptyStateSection>
-      <EmptyStateSection>
-        <Heading elementType="h2">Headline</Heading>
-        <Text>
-          In publishing and graphic design, lorem ipsum is common placeholder text used to demonstrate the graphic
-          elements
-        </Text>
-      </EmptyStateSection>
-    </EmptyState>
-  ),
-});
-
-figma.connect(EmptyState, EMPTY_STATE_NODE_URL, {
-  props: {},
-  variant: {
-    'Show Buttons': false,
-    'Show Illustration': false,
-    'Show Link': true,
-  },
-  example: (props) => (
-    <EmptyState {...props}>
-      <EmptyStateSection>
-        <Heading elementType="h2">Headline</Heading>
-        <Text>
-          In publishing and graphic design, lorem ipsum is common placeholder text used to demonstrate the graphic
-          elements
-        </Text>
-      </EmptyStateSection>
-      <EmptyStateSection>
-        <Link href="#link">Link to something</Link>
-      </EmptyStateSection>
-    </EmptyState>
-  ),
-});
-
-figma.connect(EmptyState, EMPTY_STATE_NODE_URL, {
-  props: {},
-  variant: {
-    'Show Buttons': true,
-    'Show Illustration': true,
-    'Show Link': false,
-  },
-  example: (props) => (
-    <EmptyState {...props}>
-      <EmptyStateSection>Replace with your own illustration content</EmptyStateSection>
-      <EmptyStateSection>
-        <Heading elementType="h2">Headline</Heading>
-        <Text>
-          In publishing and graphic design, lorem ipsum is common placeholder text used to demonstrate the graphic
-          elements
-        </Text>
-      </EmptyStateSection>
-      <EmptyStateSection>
-        <ActionGroup alignmentX={{ mobile: 'stretch', tablet: 'center' }}>
-          <ButtonLink href="#">Button</ButtonLink>
-          <ButtonLink href="#" color="secondary">
-            Button
-          </ButtonLink>
-        </ActionGroup>
-      </EmptyStateSection>
-    </EmptyState>
-  ),
-});
-
-figma.connect(EmptyState, EMPTY_STATE_NODE_URL, {
-  props: {},
-  variant: {
-    'Show Buttons': true,
-    'Show Illustration': false,
-    'Show Link': true,
-  },
-  example: (props) => (
-    <EmptyState {...props}>
-      <EmptyStateSection>
-        <Heading elementType="h2">Headline</Heading>
-        <Text>
-          In publishing and graphic design, lorem ipsum is common placeholder text used to demonstrate the graphic
-          elements
-        </Text>
-      </EmptyStateSection>
-      <EmptyStateSection>
-        <ActionGroup alignmentX={{ mobile: 'stretch', tablet: 'center' }}>
-          <ButtonLink href="#">Button</ButtonLink>
-          <ButtonLink href="#" color="secondary">
-            Button
-          </ButtonLink>
-        </ActionGroup>
-      </EmptyStateSection>
-      <EmptyStateSection>
-        <Link href="#link">Link to something</Link>
-      </EmptyStateSection>
-    </EmptyState>
-  ),
-});
-
-figma.connect(EmptyState, EMPTY_STATE_NODE_URL, {
-  props: {},
-  variant: {
-    'Show Buttons': false,
-    'Show Illustration': true,
-    'Show Link': true,
-  },
-  example: (props) => (
-    <EmptyState {...props}>
-      <EmptyStateSection>Replace with your own illustration content</EmptyStateSection>
-      <EmptyStateSection>
-        <Heading elementType="h2">Headline</Heading>
-        <Text>
-          In publishing and graphic design, lorem ipsum is common placeholder text used to demonstrate the graphic
-          elements
-        </Text>
-      </EmptyStateSection>
-      <EmptyStateSection>
-        <Link href="#link">Link to something</Link>
-      </EmptyStateSection>
-    </EmptyState>
-  ),
-});
-
-figma.connect(EmptyState, EMPTY_STATE_NODE_URL, {
-  props: {},
-  variant: {
-    'Show Buttons': true,
-    'Show Illustration': true,
-    'Show Link': true,
-  },
-  example: (props) => (
-    <EmptyState {...props}>
-      <EmptyStateSection>Replace with your own illustration content</EmptyStateSection>
-      <EmptyStateSection>
-        <Heading elementType="h2">Headline</Heading>
-        <Text>
-          In publishing and graphic design, lorem ipsum is common placeholder text used to demonstrate the graphic
-          elements
-        </Text>
-      </EmptyStateSection>
-      <EmptyStateSection>
-        <ActionGroup alignmentX={{ mobile: 'stretch', tablet: 'center' }}>
-          <ButtonLink href="#">Button</ButtonLink>
-          <ButtonLink href="#" color="secondary">
-            Button
-          </ButtonLink>
-        </ActionGroup>
-      </EmptyStateSection>
-      <EmptyStateSection>
-        <Link href="#link">Link to something</Link>
-      </EmptyStateSection>
+      {buttons}
+      {link}
     </EmptyState>
   ),
 });
