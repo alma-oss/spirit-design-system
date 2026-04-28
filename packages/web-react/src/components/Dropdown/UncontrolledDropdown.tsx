d740c92930d8360f1d797decf06dd8145d940963
Pavel Klibani <pavel.klibani@almamedia.com>
2026-04-28 15:55:26 +0200

    feat(web-react): add dropdown popover keyboard support #DS-2501

--- packages/web-react/src/components/Dropdown/UncontrolledDropdown.tsx
+++ packages/web-react/src/components/Dropdown/UncontrolledDropdown.tsx
@@ -1,29 +1,74 @@
 'use client';
 
 import classNames from 'classnames';
-import React, { useRef } from 'react';
-import { useStyleProps } from '../../hooks';
+import React, { type KeyboardEventHandler, type MutableRefObject, useCallback, useRef } from 'react';
+import { useOverlay, useStyleProps } from '../../hooks';
 import { type UncontrolledDropdownProps } from '../../types';
 import { DropdownProvider } from './DropdownContext';
 import { useDropdown } from './useDropdown';
 import { useDropdownStyleProps } from './useDropdownStyleProps';
 
 const UncontrolledDropdown = (props: UncontrolledDropdownProps) => {
-  const { children, enableAutoClose = true, fullWidthMode, id, onAutoClose, placement, ...rest } = props;
+  const {
+    children,
+    enableAutoClose = true,
+    fullWidthMode,
+    id,
+    onAutoClose,
+    placement,
+    triggerRef: externalTriggerRef,
+    ...rest
+  } = props;
   const { classProps, props: modifiedProps } = useDropdownStyleProps(rest);
-  const { styleProps, props: otherProps } = useStyleProps(modifiedProps);
+  const { styleProps, props: transferProps } = useStyleProps(modifiedProps);
+  const { onKeyDown: onUserKeyDown, ...otherProps } = transferProps;
 
   const dropdownRef = useRef<HTMLDivElement>(null);
-  const triggerRef = useRef<HTMLDivElement>();
+  const triggerRef = useRef<HTMLElement | null>(null);
+  const mergedTriggerRef: MutableRefObject<HTMLElement | null | undefined> = externalTriggerRef ?? triggerRef;
 
-  const { isOpen, toggleHandler: onToggle } = useDropdown({ dropdownRef, triggerRef, enableAutoClose, onAutoClose });
+  const { isOpen, toggleHandler: onToggle } = useDropdown({
+    dropdownRef,
+    triggerRef: mergedTriggerRef,
+    enableAutoClose,
+    onAutoClose,
+  });
+  const closeOverlay = useCallback(() => {
+    if (!isOpen) {
+      return;
+    }
+
+    onToggle();
+
+    queueMicrotask(() => {
+      mergedTriggerRef.current?.focus();
+    });
+  }, [isOpen, mergedTriggerRef, onToggle]);
+
+  const { onOverlayKeyDown } = useOverlay({
+    isOpen,
+    overlayRef: dropdownRef,
+    onClose: closeOverlay,
+    closeOnInteractOutside: false,
+  });
+
+  const handleKeyDown: KeyboardEventHandler<HTMLElement> = useCallback(
+    (event) => {
+      onUserKeyDown?.(event);
+      onOverlayKeyDown(event);
+    },
+    [onOverlayKeyDown, onUserKeyDown],
+  );
+  const mergedProps = { ...otherProps, onKeyDown: handleKeyDown };
 
   return (
-    <DropdownProvider value={{ id, isOpen, fullWidthMode, placement, onToggle, dropdownRef, triggerRef }}>
+    <DropdownProvider
+      value={{ id, isOpen, fullWidthMode, placement, onToggle, dropdownRef, triggerRef: mergedTriggerRef }}
+    >
       <div
         ref={dropdownRef}
         {...styleProps}
-        {...otherProps}
+        {...mergedProps}
         className={classNames(classProps.root, styleProps.className)}
       >
         {children}
