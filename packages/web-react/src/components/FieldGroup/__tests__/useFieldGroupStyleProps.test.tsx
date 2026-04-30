161175e7f4ab6b24a09fde2cf118b108fae724fd
Jan Kryšpín <crishpeen@gmail.com>
2026-04-30 11:47:33 +0200

    refactor(web-react): use Flex and utilities instead of own FieldGroup styles #DS-564

--- packages/web-react/src/components/FieldGroup/__tests__/useFieldGroupStyleProps.test.tsx
+++ packages/web-react/src/components/FieldGroup/__tests__/useFieldGroupStyleProps.test.tsx
@@ -3,23 +3,8 @@ import { useFieldGroupStyleProps } from '../useFieldGroupStyleProps';
 
 describe('useFieldGroupStyleProps', () => {
   it('should return defaults', () => {
-    const props = {};
-    const { result } = renderHook(() => useFieldGroupStyleProps(props));
+    const { result } = renderHook(() => useFieldGroupStyleProps());
 
-    expect(result.current.classProps.root).toBe('FieldGroup');
-  });
-
-  it('should return FieldGroup danger class', () => {
-    const props = { validationState: 'danger' as const };
-    const { result } = renderHook(() => useFieldGroupStyleProps(props));
-
-    expect(result.current.classProps.root).toBe('FieldGroup FieldGroup--danger');
-  });
-
-  it('should return fields class', () => {
-    const props = {};
-    const { result } = renderHook(() => useFieldGroupStyleProps(props));
-
-    expect(result.current.classProps.fields).toBe('FieldGroup__fields');
+    expect(result.current.classProps.root).toBe('border-0');
   });
 });
