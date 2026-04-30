57c18e93b5fbb972b7d9749c300e76bb4c6d9035
Jan Kryšpín <crishpeen@gmail.com>
2026-04-30 16:50:54 +0200

    refactor(web-react): use InputContainer and InputAddon in form fields #DS-2548
    Introduce InputContainer, InputAddon and CharacterCounter components
    and refactor TextFieldBase and Select to use them.

---
+++ packages/web-react/src/components/InputAddon/demo/InputAddonCurrency.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { InputContainer } from '../../InputContainer';
+import { Label } from '../../Label';
+import { VisuallyHidden } from '../../VisuallyHidden';
+import InputAddon from '../InputAddon';
+
+const InputAddonCurrency = () => (
+  <>
+    <div>
+      <Label htmlFor="input-addon-currency-eur">Amount</Label>
+      <InputContainer size="medium">
+        <InputAddon elementType="label" htmlFor="input-addon-currency-eur">
+          <span aria-hidden="true">€</span>
+          <VisuallyHidden>in EUR</VisuallyHidden>
+        </InputAddon>
+        <input
+          type="text"
+          id="input-addon-currency-eur"
+          name="inputAddonCurrencyEur"
+          placeholder="0,00"
+          inputMode="decimal"
+          autoComplete="transaction-amount"
+        />
+      </InputContainer>
+    </div>
+
+    <div>
+      <Label htmlFor="input-addon-currency-usd">Amount</Label>
+      <InputContainer size="medium">
+        <InputAddon elementType="label" htmlFor="input-addon-currency-usd">
+          <span aria-hidden="true">$</span>
+          <VisuallyHidden>in USD</VisuallyHidden>
+        </InputAddon>
+        <input
+          type="text"
+          id="input-addon-currency-usd"
+          name="inputAddonCurrencyUsd"
+          placeholder="0.00"
+          inputMode="decimal"
+          autoComplete="transaction-amount"
+        />
+      </InputContainer>
+    </div>
+
+    <div>
+      <Label htmlFor="input-addon-currency-czk">Amount</Label>
+      <InputContainer size="medium">
+        <input
+          type="text"
+          id="input-addon-currency-czk"
+          name="inputAddonCurrencyCzk"
+          placeholder="0,00"
+          inputMode="decimal"
+          autoComplete="transaction-amount"
+        />
+        <InputAddon elementType="label" htmlFor="input-addon-currency-czk">
+          <span aria-hidden="true">Kč</span>
+          <VisuallyHidden>in CZK</VisuallyHidden>
+        </InputAddon>
+      </InputContainer>
+    </div>
+  </>
+);
+
+export default InputAddonCurrency;
