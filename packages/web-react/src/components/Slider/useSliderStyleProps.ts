19d5fefd9bd793e26f4c16fab66a8e715c6a495a
Jan Kryšpín <crishpeen@gmail.com>
2026-04-30 08:57:48 +0200

    refactor(web-react): use updated Slider styles #DS-564

--- packages/web-react/src/components/Slider/useSliderStyleProps.ts
+++ packages/web-react/src/components/Slider/useSliderStyleProps.ts
@@ -6,27 +6,22 @@ type UseSliderStyleProps = Omit<SpiritSliderProps, 'id' | 'value' | 'label'>;
 
 export interface SliderStyles {
   classProps: {
-    root: string;
     input: string;
   };
   props: UseSliderStyleProps;
 }
 
 export function useSliderStyleProps(props: UseSliderStyleProps): SliderStyles {
-  const { isDisabled, validationState, ...restProps } = props;
+  const { isDisabled, ...restProps } = props;
 
   const sliderClass = useClassNamePrefix('Slider');
   const isDisabledClass = `${sliderClass}--disabled`;
-  const validationStateClass = `${sliderClass}--${validationState}`;
-  const inputClass = `${sliderClass}__input`;
 
   return {
     classProps: {
-      root: classNames(sliderClass, {
+      input: classNames(sliderClass, {
         [isDisabledClass]: isDisabled,
-        [validationStateClass]: validationState,
       }),
-      input: inputClass,
     },
     props: restProps,
   };
