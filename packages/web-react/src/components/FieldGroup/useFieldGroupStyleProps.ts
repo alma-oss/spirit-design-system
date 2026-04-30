7ffd5e8fd40641faf1ff85b58c12f115ca5489b9
Jan Kryšpín <crishpeen@gmail.com>
2026-04-30 11:47:33 +0200

    refactor(web-react): use Flex and utilities instead of own FieldGroup styles #DS-564

--- packages/web-react/src/components/FieldGroup/useFieldGroupStyleProps.ts
+++ packages/web-react/src/components/FieldGroup/useFieldGroupStyleProps.ts
@@ -1,28 +1,14 @@
-import classNames from 'classnames';
 import { useClassNamePrefix } from '../../hooks';
-import { type Validation } from '../../types';
-
-export interface UseFieldGroupStyleProps extends Validation {}
 
 export interface UseFieldGroupStyleReturn {
   /** className props */
   classProps: {
     root: string;
-    fields: string;
   };
 }
 
-export const useFieldGroupStyleProps = ({ validationState }: UseFieldGroupStyleProps): UseFieldGroupStyleReturn => {
-  const fieldGroupClass = useClassNamePrefix('FieldGroup');
-  const fieldGroupValidationClass = `${fieldGroupClass}--${validationState}`;
-  const fieldGroupFieldsClass = `${fieldGroupClass}__fields`;
-
-  return {
-    classProps: {
-      root: classNames(fieldGroupClass, {
-        [fieldGroupValidationClass]: validationState,
-      }),
-      fields: fieldGroupFieldsClass,
-    },
-  };
-};
+export const useFieldGroupStyleProps = (): UseFieldGroupStyleReturn => ({
+  classProps: {
+    root: useClassNamePrefix('border-0'),
+  },
+});
