5c9ad80357b67f90547a23c9b08689f81723d271
literat <tomas@litera.me>
2026-04-30 17:10:38 +0200

    docs(web-react): make some components renderable in docsite preview

---
+++ packages/web-react/src/components/Header/demo/preview.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+// @ts-ignore: No declaration file -- @see https://jira.almacareer.tech/browse/DS-561
+import icons from '@alma-oss/spirit-icons/icons';
+import React, { StrictMode } from 'react';
+import DocsSection from '../../../../docs/DocsSection';
+import { IconsProvider } from '../../../context';
+import HeaderExperimentalRTL from './HeaderExperimentalRTL';
+import HeaderMinimal from './HeaderMinimal';
+import HeaderSimple from './HeaderSimple';
+import HeaderSimpleTransparent from './HeaderSimpleTransparent';
+import HeaderWithActions from './HeaderWithActions';
+import HeaderWithActionsAndDialog from './HeaderWithActionsAndDialog';
+
+export const Preview = () => (
+  <StrictMode>
+    <IconsProvider value={icons}>
+      <DocsSection title="Minimal Header" stackAlignment="stretch" container="heading-only">
+        <HeaderMinimal />
+      </DocsSection>
+      <DocsSection title="Header with Actions" stackAlignment="stretch" container="heading-only">
+        <HeaderWithActions />
+      </DocsSection>
+      <DocsSection title="Header with Actions and Header Dialog" stackAlignment="stretch" container="heading-only">
+        <HeaderWithActionsAndDialog />
+      </DocsSection>
+      <DocsSection title="Simple Header" stackAlignment="stretch" container="heading-only">
+        <HeaderSimple />
+      </DocsSection>
+      <DocsSection title="Simple Transparent Header" stackAlignment="stretch" container="heading-only">
+        <HeaderSimpleTransparent />
+      </DocsSection>
+      <DocsSection title="Experimental RTL Support" stackAlignment="stretch">
+        <HeaderExperimentalRTL />
+      </DocsSection>
+    </IconsProvider>
+  </StrictMode>
+);
