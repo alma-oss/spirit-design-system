de43ac7fa64417618820eb95912f3ad27fd35efc
Jan Kryšpín <crishpeen@gmail.com>
2026-04-30 08:57:48 +0200

    refactor(web-react): use updated Slider styles #DS-564

--- packages/web-react/src/components/Slider/__tests__/useSliderStyleProps.test.tsx
+++ packages/web-react/src/components/Slider/__tests__/useSliderStyleProps.test.tsx
@@ -11,19 +11,12 @@ describe('useSliderStyleProps', () => {
   it('should return defaults', () => {
     const { result } = renderHook(() => useSliderStyleProps(defaultProps));
 
-    expect(result.current.classProps.root).toBe('Slider');
-    expect(result.current.classProps.input).toBe('Slider__input');
+    expect(result.current.classProps.input).toBe('Slider');
   });
 
   it('should return disabled class', () => {
     const { result } = renderHook(() => useSliderStyleProps({ ...defaultProps, isDisabled: true }));
 
-    expect(result.current.classProps.root).toContain('Slider--disabled');
-  });
-
-  it('should return validation state class', () => {
-    const { result } = renderHook(() => useSliderStyleProps({ ...defaultProps, validationState: 'danger' }));
-
-    expect(result.current.classProps.root).toContain('Slider--danger');
+    expect(result.current.classProps.input).toContain('Slider--disabled');
   });
 });
