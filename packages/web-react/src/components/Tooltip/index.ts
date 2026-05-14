1a1d5db716d2c4d980365a7075576092f0d1041c
Jan Kryšpín <crishpeen@gmail.com>
2026-05-14 22:36:18 +0200

    feat(web,web-react)!: migrate `TooltipPopover` to neutral color-scheme
    BREAKING CHANGE: TooltipPopover now requires color-scheme helper for surface colors

--- packages/web-react/src/components/Tooltip/index.ts
+++ packages/web-react/src/components/Tooltip/index.ts
@@ -3,5 +3,6 @@ export { default as TooltipCloseButton } from './TooltipCloseButton';
 export { default as TooltipPopover } from './TooltipPopover';
 export { default as TooltipTrigger } from './TooltipTrigger';
 export { default as UncontrolledTooltip } from './UncontrolledTooltip';
+export * from './constants';
 export * from './useTooltip';
 export * from './useTooltipStyleProps';
