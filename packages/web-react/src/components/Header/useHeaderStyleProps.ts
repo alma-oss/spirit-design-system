de768eee937582d489ec01e0f992acf5754035f6
Kateřina Klouček Dlouhá <katerina.kloucek.dlouha@almamedia.com>
2026-06-15 16:04:16 +0200

    feat(web-react)!: stabilize UNSTABLE_Header and remove deprecated Header

--- packages/web-react/src/components/Header/useHeaderStyleProps.ts
+++ packages/web-react/src/components/Header/useHeaderStyleProps.ts
@@ -1,99 +1,29 @@
 import classNames from 'classnames';
 import { useClassNamePrefix } from '../../hooks';
-import { type HeaderActionsColorType, type HeaderColorType } from '../../types';
-import { HEADER_ACTIONS_COLOR_DEFAULT, HEADER_COLOR_DEFAULT } from './constants';
+import { type SpiritHeaderProps } from '../../types';
 
-export interface UseHeaderStyleProps {
-  color?: HeaderColorType;
-  hasActionsAtEnd?: boolean;
-  isSimple?: boolean;
-  actionsColor?: HeaderActionsColorType;
-  isCurrentLink?: boolean;
-}
-export interface UseHeaderStyleReturn {
+export interface HeaderStyles<T> {
   classProps: {
     root: string;
-    headerButton: string;
-    headerDesktopActions: string;
-    headerDialog: {
-      root: string;
-      panel: string;
-      content: string;
-    };
-    headerDialogActions: string;
-    headerDialogButton: string;
-    headerDialogCloseButton: string;
-    headerDialogLink: string;
-    headerDialogNav: string;
-    headerDialogNavItem: string;
-    headerDialogText: string;
-    headerLink: string;
-    headerMobileActions: string;
-    headerNav: string;
-    headerNavItem: string;
+    logo: string;
   };
+  props: T;
 }
 
-export const useHeaderStyleProps = (
-  {
-    actionsColor = HEADER_ACTIONS_COLOR_DEFAULT,
-    color = HEADER_COLOR_DEFAULT,
-    hasActionsAtEnd,
-    isCurrentLink,
-    isSimple,
-  }: UseHeaderStyleProps = {
-    actionsColor: HEADER_ACTIONS_COLOR_DEFAULT,
-    color: HEADER_COLOR_DEFAULT,
-    hasActionsAtEnd: false,
-    isCurrentLink: false,
-    isSimple: false,
-  },
-): UseHeaderStyleReturn => {
-  const headerClass = useClassNamePrefix('Header');
-  const headerColorClass = `${headerClass}--${color}`;
-  const headerSimpleClass = `${headerClass}--simple`;
-  const headerNavClass = `${headerClass}Nav`;
-  const headerNavItemClass = `${headerClass}NavItem`;
-  const headerLinkClass = `${headerClass}Link`;
-  const headerLinkCurrentClass = `${headerLinkClass}--current`;
-  const headerDesktopActionsClass = `${headerClass}DesktopActions`;
-  const headerDesktopActionsAtEndClass = `${headerDesktopActionsClass}--end`;
-  const headerMobileActionsClass = `${headerClass}MobileActions`;
-  const headerDialogClass = `${headerClass}Dialog`;
-  const headerDialogPanelClass = `${headerDialogClass}__panel`;
-  const headerDialogContentClass = `${headerDialogClass}__content`;
-  const headerDialogActionsClass = `${headerClass}DialogActions`;
-  const headerDialogActionsColorClass = `${headerDialogActionsClass}--${actionsColor}`;
-  const headerDialogCloseButtonClass = `${headerClass}DialogCloseButton`;
-  const headerDialogLinkClass = `${headerClass}DialogLink`;
-  const headerDialogLinkCurrentClass = `${headerDialogLinkClass}--current`;
-  const headerDialogNavClass = `${headerClass}DialogNav`;
-  const headerDialogNavItemClass = `${headerClass}DialogNavItem`;
-  const headerDialogTextClass = `${headerClass}DialogText`;
+export const useHeaderStyleProps = (props: SpiritHeaderProps): HeaderStyles<SpiritHeaderProps> => {
+  const { hasBottomDivider = false, ...restProps } = props;
 
-  const classProps = {
-    root: classNames(headerClass, headerColorClass, { [headerSimpleClass]: isSimple }),
-    headerButton: headerLinkClass,
-    headerDesktopActions: classNames(headerDesktopActionsClass, { [headerDesktopActionsAtEndClass]: hasActionsAtEnd }),
-    headerDialog: {
-      root: headerDialogClass,
-      panel: headerDialogPanelClass,
-      content: headerDialogContentClass,
-    },
-    headerDialogActions: classNames(headerDialogActionsClass, headerDialogActionsColorClass),
-    headerDialogButton: headerDialogLinkClass,
-    headerDialogCloseButton: headerDialogCloseButtonClass,
-    headerDialogLink: classNames(headerDialogLinkClass, { [headerDialogLinkCurrentClass]: isCurrentLink }),
-    headerDialogNav: headerDialogNavClass,
-    headerDialogNavItem: headerDialogNavItemClass,
-    headerDialogText: headerDialogTextClass,
-    headerLink: classNames(headerLinkClass, { [headerLinkCurrentClass]: isCurrentLink }),
-    headerMobileActions: headerMobileActionsClass,
-    headerNav: headerNavClass,
-    headerNavItem: headerNavItemClass,
-  };
+  const headerClass = useClassNamePrefix('Header');
+  const headerLogoClass = `${headerClass}Logo`;
+  const headerHasBottomDividerClass = `${headerClass}--bottomDivider`;
 
   return {
-    classProps,
+    classProps: {
+      root: classNames(headerClass, {
+        [headerHasBottomDividerClass]: hasBottomDivider,
+      }),
+      logo: headerLogoClass,
+    },
+    props: restProps,
   };
 };
