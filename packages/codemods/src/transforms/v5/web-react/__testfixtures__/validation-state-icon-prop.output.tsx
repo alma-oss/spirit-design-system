aa4e6b88834c2413037d4fb4b920a5df1a2330ba
Pavel Klibani <pavel.klibani@almamedia.com>
2026-07-02 13:39:16 +0200

    feat(codemods): add `validation-state-icon-prop` codemod for `ValidationText`
    Rename `hasValidationStateIcon` to `validationStateIcon` on standalone usage.

---
+++ packages/codemods/src/transforms/v5/web-react/__testfixtures__/validation-state-icon-prop.output.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// @ts-ignore: No declaration -- The library is not installed; we don't need to install it for fixtures.
+import { TextField, ValidationText } from '@alma-oss/spirit-web-react';
+
+export const MyComponent = () => (
+  <>
+    <ValidationText validationText="Saved successfully." validationStateIcon="success" />
+    <ValidationText validationText="Required field." />
+    <TextField id="name" label="Name" hasValidationIcon validationState="danger" />
+  </>
+);
---
+++ packages/codemods/src/transforms/v5/web-react/__tests__/validation-state-icon-prop.test.ts
@@ -0,0 +1,3 @@
+import { testTransform } from '../../../../../tests/testUtils';
+
+testTransform(__dirname, 'validation-state-icon-prop');
