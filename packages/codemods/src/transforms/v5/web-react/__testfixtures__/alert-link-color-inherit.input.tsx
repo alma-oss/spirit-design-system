7cc9e7ccd4e66ea36716efb5141f1052be7aa89b
Kateřina Klouček Dlouhá <katerina.kloucek.dlouha@almamedia.com>
2026-07-02 14:18:43 +0200

    docs(repo): document inherit link migration in the Alert #DS-2379

---
+++ packages/codemods/src/transforms/v5/web-react/__testfixtures__/alert-link-color-inherit.input.tsx
@@ -0,0 +1,33 @@
+// @ts-nocheck
+import React from 'react';
+// @ts-ignore: No declaration -- The library is not installed; we don't need to install it for fixtures.
+import { Alert, Link } from '@alma-oss/spirit-web-react';
+
+export const MyComponent = () => (
+  <>
+    {/* Link inside Alert without color prop — should get color="inherit" */}
+    <Alert color="success">
+      See <Link href="/faq">FAQ</Link> for more info.
+    </Alert>
+
+    {/* Link inside Alert that already has color="inherit" — should not change */}
+    <Alert color="warning">
+      See <Link href="/faq" color="inherit">FAQ</Link> for more info.
+    </Alert>
+
+    {/* Link inside Alert that already has a different color — should not change */}
+    <Alert color="danger">
+      See <Link href="/faq" color="primary">FAQ</Link> for more info.
+    </Alert>
+
+    {/* Deeply nested Link inside Alert — should get color="inherit" */}
+    <Alert color="informative">
+      <div>
+        <span>See <Link href="/faq">FAQ</Link> for more info.</span>
+      </div>
+    </Alert>
+
+    {/* Link outside of Alert — should not change */}
+    <Link href="/other">Other link</Link>
+  </>
+);
