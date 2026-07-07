ffc8ea2914c65894a4c3b9c1dded5b7e39767f27
Kateřina Řehořková <149075264+katerinarehorkova@users.noreply.github.com>
2026-07-07 12:39:11 +0200

    docs(web-react): make FieldGroup radio and checkbox demos interactive
    Controlled isChecked props without onChange handlers prevented toggling in the grouped demos.

--- packages/web-react/src/components/FieldGroup/demo/FieldGroupGroupedRadios.tsx
+++ packages/web-react/src/components/FieldGroup/demo/FieldGroupGroupedRadios.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Radio } from '../../Radio';
 import FieldGroup from '../FieldGroup';
 
-const FieldGroupGroupedRadios = () => (
-  <FieldGroup id="field-group-grouped-radios" label="Label">
-    <Radio id="radio-1" label="Radio Label" name="radioDefault" isChecked />
-    <Radio id="radio-2" label="Radio Label" name="radioDefault" />
-  </FieldGroup>
-);
+const FieldGroupGroupedRadios = () => {
+  const [selectedRadio, setSelectedRadio] = useState('radio-1');
+
+  return (
+    <FieldGroup id="field-group-grouped-radios" label="Label">
+      <Radio
+        id="radio-1"
+        label="Radio Label"
+        name="radioDefault"
+        isChecked={selectedRadio === 'radio-1'}
+        onChange={() => setSelectedRadio('radio-1')}
+      />
+      <Radio
+        id="radio-2"
+        label="Radio Label"
+        name="radioDefault"
+        isChecked={selectedRadio === 'radio-2'}
+        onChange={() => setSelectedRadio('radio-2')}
+      />
+    </FieldGroup>
+  );
+};
 
 export default FieldGroupGroupedRadios;
