d3c40d0709ecbb5bcfb11f858b9068e4449693a2
Kateřina Řehořková <149075264+katerinarehorkova@users.noreply.github.com>
2026-07-07 12:39:11 +0200

    docs(web-react): make FieldGroup radio and checkbox demos interactive
    Controlled isChecked props without onChange handlers prevented toggling in the grouped demos.

--- packages/web-react/src/components/FieldGroup/demo/FieldGroupGroupedCheckboxes.tsx
+++ packages/web-react/src/components/FieldGroup/demo/FieldGroupGroupedCheckboxes.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
+import { useToggle } from '../../../hooks';
 import { Checkbox } from '../../Checkbox';
 import FieldGroup from '../FieldGroup';
 
-const FieldGroupGroupedCheckboxes = () => (
-  <FieldGroup id="field-group-grouped-checkboxes" label="Label">
-    <Checkbox id="checkbox-1" label="Checkbox Label" name="checkboxDefault" isChecked />
-    <Checkbox id="checkbox-2" label="Checkbox Label" name="checkboxDefault" />
-  </FieldGroup>
-);
+const FieldGroupGroupedCheckboxes = () => {
+  const [isFirstCheckboxChecked, toggleFirstCheckbox] = useToggle(true);
+  const [isSecondCheckboxChecked, toggleSecondCheckbox] = useToggle(false);
+
+  return (
+    <FieldGroup id="field-group-grouped-checkboxes" label="Label">
+      <Checkbox
+        id="checkbox-1"
+        label="Checkbox Label"
+        name="checkboxDefault"
+        isChecked={isFirstCheckboxChecked}
+        onChange={toggleFirstCheckbox}
+      />
+      <Checkbox
+        id="checkbox-2"
+        label="Checkbox Label"
+        name="checkboxDefault"
+        isChecked={isSecondCheckboxChecked}
+        onChange={toggleSecondCheckbox}
+      />
+    </FieldGroup>
+  );
+};
 
 export default FieldGroupGroupedCheckboxes;
