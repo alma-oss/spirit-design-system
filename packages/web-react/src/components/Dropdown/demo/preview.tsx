ae2a435afe45bd24f80561be0007396f323dc2a3
literat <tomas@litera.me>
2026-07-13 21:23:26 +0200

    docs(web-react): make partial-conflict components renderable in docsite preview
    Extract each component's demo render tree into a `demo/preview.tsx` exporting a
    `Preview` component, slim `demo/index.tsx` to a bootstrap that renders it, and
    add a `preview/index.ts` re-export for the docsite. Components:
    
    - Card, Checkbox, Drawer, Dropdown, InputDetails, Navigation, PricingPlan,
      Radio, SegmentedControl, Stack, Toast, Tooltip, File, FileUpload
    
    refs #DS-2221

---
+++ packages/web-react/src/components/Dropdown/demo/preview.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import icons from '@alma-oss/spirit-icons/icons';
+import React, { StrictMode } from 'react';
+import DocsSection from '../../../../docs/DocsSection';
+import { IconsProvider } from '../../../context';
+import DropdownAlignment from './DropdownAlignment';
+import DropdownDisabledAutoclose from './DropdownDisabledAutoclose';
+import DropdownFullwidthAll from './DropdownFullwidthAll';
+import DropdownFullwidthMobileOnly from './DropdownFullwidthMobileOnly';
+import DropdownLongerContent from './DropdownLongerContent';
+import DropdownPlacements from './DropdownPlacements';
+import DropdownVariousItems from './DropdownVariousItems';
+
+export const Preview = () => (
+  <StrictMode>
+    <IconsProvider value={icons}>
+      <DocsSection title="Placements" stackAlignment="stretch">
+        <DropdownPlacements />
+      </DocsSection>
+      <DocsSection title="Alignment" stackAlignment="stretch">
+        <DropdownAlignment />
+      </DocsSection>
+      <DocsSection title="Various items">
+        <DropdownVariousItems />
+      </DocsSection>
+      <DocsSection title="Usage with disabled auto close">
+        <DropdownDisabledAutoclose />
+      </DocsSection>
+      <DocsSection title="Longer content">
+        <DropdownLongerContent />
+      </DocsSection>
+      <DocsSection title="Full-width mode 'all'">
+        <DropdownFullwidthAll />
+      </DocsSection>
+      <DocsSection title="Full-width mode 'mobile-only'">
+        <DropdownFullwidthMobileOnly />
+      </DocsSection>
+    </IconsProvider>
+  </StrictMode>
+);
