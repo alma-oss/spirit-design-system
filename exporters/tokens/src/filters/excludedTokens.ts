9670454dda63cbaeca01b7503d288010f2c3886c
Kateřina Řehořková <149075264+katerinarehorkova@users.noreply.github.com>
2026-08-13 16:01:54 +0200

    feat(exporter-tokens): exclude italic typography tokens #DS-2725

--- exporters/tokens/src/filters/excludedTokens.ts
+++ exporters/tokens/src/filters/excludedTokens.ts
@@ -3,14 +3,16 @@ import { type Token, TokenType } from '@supernovaio/sdk-exporters';
 /* Filters */
 const isFigmaToken = (token: Token) => token.name.includes('figma-');
 const isTypographyLink = (token: Token) => token.tokenType === TokenType.typography && token.name.includes('Link');
+const isTypographyItalic = (token: Token) => token.tokenType === TokenType.typography && token.name.includes('Italic');
 
 /* Predicates */
-const excludePredicates = [isFigmaToken, isTypographyLink];
+const excludePredicates = [isFigmaToken, isTypographyLink, isTypographyItalic];
 
 /**
  * Filters out tokens that should be excluded from processing:
  * - Tokens with "figma-" in their name
  * - Typography tokens with "Link" in their name
+ * - Typography tokens with "Italic" in their name
  *
  * @param {Token[]} tokens - Array of tokens to filter.
  * @returns {Token[]} - Array of filtered tokens.
